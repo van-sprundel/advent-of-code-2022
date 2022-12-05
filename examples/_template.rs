b0e72ee85b08b03badc06749e5524d86f21f8b52
recranet <ramon@recranet.com>
2022-12-05 09:34:15 +0100

    template

---
+++ examples/_template.rs
@@ -0,0 +1,21 @@
+use anyhow::{Result};
+
+fn main() -> Result<()> {
+    println!("{}", part_1()?);
+    println!("{}", part_2()?);
+    Ok(())
+}
+
+fn part_1() -> Result<i32> {
+    EXAMPLE
+    ;
+    Ok(0)
+}
+
+fn part_2() -> Result<i32> {
+    Ok(0)
+}
+
+const EXAMPLE: &str = r#""#;
+
+const INPUT: &str = r#""#;
