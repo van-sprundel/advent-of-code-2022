cf783118f89483df6307922dc7944d55f05c441e
recranet <ramon@recranet.com>
2022-12-05 09:34:15 +0100

    template

---
+++ examples/_template.rs
@@ -0,0 +1,15 @@
+use anyhow::{Result};
+
+fn main() -> Result<()> {
+    println!("{}", part_1()?);
+    println!("{}", part_2()?);
+    Ok(())
+}
+
+fn part_1() -> Result<i32> {
+    Ok(0)
+}
+
+fn part_2() -> Result<i32> {
+    Ok(0)
+}
